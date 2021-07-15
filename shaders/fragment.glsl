daefa1b9cf13b82b5b075c2a49d2a7d6f811bacd
Baptiste Dauphouy <bdauphouy@gmail.com>
2021-07-15 20:48:05 +0200

    feat: initial commit

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,11 +1,5 @@
-uniform sampler2D uTexture;
-uniform float uTime;
-varying vec2 vUv;
-
 void main(){
 
-    float variation = sin(uTime);
-    vec4 myimage = texture(uTexture, vUv);
 
-    gl_FragColor = mix(myimage, vec4(.5, variation, cos(variation), 1.), variation);
+    gl_FragColor = vec4(1.0);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,6 +1,9 @@
-varying vec2 vUv;
+
 
 void main(){
-    vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
 }
