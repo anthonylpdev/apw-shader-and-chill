be444b4789bef5ca124db92c3925a4b3ada17ef7
Anthony LOPEZ <anthony.lopezpro@gmail.com>
2021-07-20 13:23:46 +0200

    Add loader

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 import baseTexture from './texture-02.jpg'
 import guify from 'guify'
+import gsap from 'gsap'
 
 export default class Sketch {
   constructor(options) {
@@ -19,24 +20,27 @@ export default class Sketch {
         80,
     )
     this.camera.rotation.reorder('YXZ')
-    this.camera.position.x = 0;
-    this.camera.position.z = 30;
+    this.camera.position.x = 0
+    this.camera.position.z = 30
     // this.camera.rotation.set(Math.PI * 2, 0, 0);
+
+    this.manager = new THREE.LoadingManager()
+    this.loadManager()
 
     this.scene = new THREE.Scene()
 
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
-      canvas: options.domElement
+      canvas: options.domElement,
       // alpha: true
     })
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     this.renderer.setClearColor(0xFFFFFF, 1)
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
     // this.controls.autoRotate = true;
-    this.controls.autoRotateSpeed = 2;
-    this.controls.enableDamping = true;
-    this.controls.enabled = true;
+    this.controls.autoRotateSpeed = 2
+    this.controls.enableDamping = true
+    this.controls.enabled = false
 
     this.resize()
     this.addObjects()
@@ -45,10 +49,10 @@ export default class Sketch {
     this.setUpResize()
 
     window.addEventListener('mousemove', (event) => {
-      const posX = event.offsetX / this.width;
-      const posY = event.offsetY / this.height;
-      this.material.uniforms.uPosX.value = posX;
-      this.material.uniforms.uPosY.value = -posY + 1;
+      const posX = event.offsetX / this.width
+      const posY = event.offsetY / this.height
+      this.material.uniforms.uPosX.value = posX
+      this.material.uniforms.uPosY.value = -posY + 1
     })
 
     // this.gui()
@@ -56,10 +60,10 @@ export default class Sketch {
   }
 
   addObjects() {
+    this.loader = new THREE.TextureLoader(this.manager)
 
-    this.loader = new THREE.TextureLoader();
-
-    this.geometry = new THREE.PlaneBufferGeometry(this.width, this.height, 100, 100)
+    this.geometry = new THREE.PlaneBufferGeometry(this.width, this.height, 100,
+        100)
     this.material = new THREE.ShaderMaterial({
       uniforms: {
         uTime: {value: 0.0},
@@ -85,7 +89,7 @@ export default class Sketch {
     this.material.uniforms.uTime.value = this.clock.getElapsedTime()
     requestAnimationFrame(this.render.bind(this))
 
-    this.controls.update();
+    this.controls.update()
 
     this.renderer.render(this.scene, this.camera)
   }
@@ -100,7 +104,7 @@ export default class Sketch {
   }
 
   applyNoise(val) {
-    this.material.uniforms.uApply.value = val;
+    this.material.uniforms.uApply.value = val
   }
 
   setUpResize() {
@@ -109,10 +113,10 @@ export default class Sketch {
 
   gui() {
     this.gui = new guify({
-      title: "Some Title",
+      title: 'Some Title',
       align: 'right',
       // theme: 'light'
-    });
+    })
 
     this.gui.Register([
       {
@@ -121,12 +125,73 @@ export default class Sketch {
         min: 0,
         max: 1,
         step: 0.01,
-        onChange: this.applyNoise.bind(this)
-      }
-    ]);
+        onChange: this.applyNoise.bind(this),
+      },
+    ])
   }
 
+  loadManager() {
 
+    this.manager.onLoad = (event) => {
+      console.log('Loading complete!')
+
+      gsap.timeline().to('#loading-label', {
+        alpha: 0,
+        y: -20,
+        duration: 1,
+        ease: 'power4.in',
+      }).fromTo('#start-label', {
+        alpha: 0,
+        y: 40,
+      }, {
+        alpha: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power4.out',
+      }).play()
+
+    }
+    this.manager.onStart = (url, itemsLoaded, itemsTotal) => {
+
+      gsap.timeline().fromTo('#loading-label', {
+        alpha: 0,
+        y: 40,
+      }, {
+        alpha: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power4.out',
+      }).play()
+
+    }
+
+    document.querySelector('#start-label').
+        addEventListener('click', (event) => {
+          event.preventDefault()
+          gsap.timeline().to('.loading-ended', {
+            alpha: 0,
+            ease: 'power4.out',
+          }).to('#loader-container', {
+            x: '-50%',
+            duration: 1,
+            ease: 'power4.out',
+          }).to('#loader-container', {
+            alpha: 0,
+            x: '-100%',
+            duration: 0,
+          }).fromTo('main .hide-during-load', {
+            alpha: 0,
+            x: 80,
+          }, {
+            alpha: 1,
+            x: 0,
+            duration: 1,
+            ease: 'power4.out',
+            stagger: 0.1,
+          }).play()
+        })
+
+  }
 }
 
 new Sketch({
